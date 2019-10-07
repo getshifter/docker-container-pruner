320a0c7333b68127a98559164536bb0ec8227e01
sawanoboly <sawanoboriyu@higanworks.com>
2019-10-07 14:57:00 +0900

    init

---
+++ sch_prune.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+prune_before=${PRUNE_BEFORE:-2h}
+interval=${INTERVAL:-1h}
+
+while true; do docker container prune --filter "until=${prune_before}" ; sleep "${interval}" ; done
+
+exit 0
